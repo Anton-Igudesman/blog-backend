a5ec5576dc51ab6e33f53b96c406d89893fcc18f
Anton Igudesman <antonguy@gmail.com>
2023-02-23 05:55:22 -0800

    changes

--- controllers/posts/postCtrl.js
+++ controllers/posts/postCtrl.js
@@ -1,7 +1,7 @@
 const Post = require("../../models/Post/Post");
 const User = require("../../models/User/User");
 const appError = require("../../utils/appError");
-const ObjectId = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 //create post
 const postCtrl = async (req, res, next) => {
